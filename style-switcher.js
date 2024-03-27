607dc87984a5d471c6f0d9f4629bdb0bbb557334
Haps <thiamhapstou@gmail.com>
2024-03-27 23:08:22 +0000

    confugaration de l'email

--- style-switcher.js
+++ style-switcher.js
@@ -48,3 +48,22 @@ window.addEventListener("load", () =>{
     }
 }
 )
+/*gerer la partie contact*/
+function SendMail(event) {
+    event.preventDefault(); // Empêche le comportement de soumission par défaut du formulaire
+
+    var params = {
+        from_name: document.getElementById("fullName").value,
+        email_id: document.getElementById("email_id").value,
+        message: document.getElementById("message").value
+    };
+
+    emailjs.send("service_8aixyoc", "template_tg89xys", params)
+        .then(function (res) {
+            alert("Success! " + res.status);
+        })
+        .catch(function (error) {
+            console.error("Failed... ", error);
+            alert("Failed to send email. Please try again later.");
+        });
+}
